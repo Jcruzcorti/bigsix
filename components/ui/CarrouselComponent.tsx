9334a1c27e0cfc7dcf8ebc9034c2e229d9cbe916
Juan Cruz Cortinas <jcruzcorti@gmail.com>
2024-01-19 16:41:49 -0300

    Added real photos to the carousel and fixed next js image bug

--- components/ui/CarrouselComponent.tsx
+++ components/ui/CarrouselComponent.tsx
@@ -2,42 +2,57 @@ import Image from 'next/image'
 import React from 'react'
 
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "@/components/ui/carousel"
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 import { Card, CardContent } from "@/components/ui/card"
 
 
 export function CarrouselComponent() {
 
-    const carrouselImages: { tittle: string; img: string; key: number}[] = [
-        {   
-            key: 1,
-            tittle: 'a',
-            img:'/bigsixphoto2.jpg'    
-        },
-        {
-            key: 2,
-            tittle: 'b',
-            img:'/bigsixphoto2.jpg'
-        },
-        {
-            key: 3,
-            tittle: 'c',
-            img:'/bigsixphoto2.jpg'
-        },
-    ]
+  const carrouselImages: { tittle: string; img: string; key: number }[] = [
+    {
+      key: 1,
+      tittle: 'Big six',
+      img: '/bigsixphoto2.jpg'
+    },
+    {
+      key: 2,
+      tittle: 'Arsenal',
+      img: '/arsenalwin.jpg'
+    },
+    {
+      key: 3,
+      tittle: 'Chelsea',
+      img: '/chelseawin.jpg'
+    },
+    {
+      key: 4,
+      tittle: 'Liverpool',
+      img: '/liverpoolwin.jpeg'
+    },
+    {
+      key: 5,
+      tittle: 'Man city',
+      img: '/mancitywin.jpg'
+    },
+    {
+      key: 6,
+      tittle: 'Man united',
+      img: '/manuniwin3.jpg'
+    },
+
+  ]
 
   return (
 
-     <div className="relative flex-col py-14 place-items-center ">
-
-                <Carousel className="w-full  max-w-xl ">
-                    <CarouselContent >
-                    {/* {Array.from({ length: 5 }).map((_, index) => (
+    <div className="relative flex-col py-20 place-items-center ">
+      <Carousel className="w-full  max-w-xl ">
+        <CarouselContent >
+          {/* {Array.from({ length: 5 }).map((_, index) => (
                         <CarouselItem key={index}>
                             <div className="p-1">
                             <Card>
@@ -48,39 +63,33 @@ export function CarrouselComponent() {
                             </div>
                         </CarouselItem>
                     ))} */}
-                        {carrouselImages.map((item) => (
-                            <CarouselItem key={item.key} className="">
-                              <div className="p-4">
-                                <Card className="bg-background ">
-                                  <CardContent className="flex items-center justify-center p-3">
-                                  <Image
-                                    className="relative rounded-xs"
-                                    src={item.img}
-                                    alt="Premier League logo"
-                                    width={500}
-                                    height={40}
-                                    priority
-                                />
-                                  </CardContent>
-                                </Card>
-                              </div>
-                            </CarouselItem>
-                          ))}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                      </Carousel>
-                     {/* <Image
-                        className="relative "
-                        src="/bigsixphoto2.jpg"
-                        alt="Premier League logo"
-                        width={400}
-                        height={40}
-                        priority
-                    /> */}
-            
+          {carrouselImages.map((item) => (
+            <CarouselItem key={item.key} className="">
+              <div className=" p-1 lg:p-4 " >
+                <Card className="bg-background ">
+                  <CardContent className="flex items-center justify-center p-3 lg:p-3">
+                    {/* <div className="h-full w-full overflow-hidden"> */}
+                    <Image
+                      className="relative rounded-xs w-full h-auto"
+                      src={item.img}
+                      alt={item.tittle}
+                      width="0"
+                      height="0"
+                      sizes="100vw"
+        
+                    />
+                    {/* </div> */}
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
 
-      </div>
+    </div>
   )
 }
 
--- components/ui/NavBarComponent.tsx
+++ components/ui/NavBarComponent.tsx
@@ -6,11 +6,12 @@ export default function NavBarComponent() {
   return (
     <div className="z-10 max-w-5xl w-full flex items-center justify-between lg:flex ">
         <Image
-        className="relative drop-shadow-md dark:drop-shadow-[0_0_0.2rem_#ffffff30] "
+        className="relative drop-shadow-md dark:drop-shadow-[0_0_0.2rem_#ffffff30] w-16 lg:w-20 h-auto"
         src="/premierlogo.png"
         alt="Premier League logo"
-        width={90}
-        height={20}
+        width="0"
+        height="0"
+        sizes='100vw'
         priority
         />
         <NavigationMenuDemo/>
